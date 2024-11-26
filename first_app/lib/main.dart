d2c45a9f93ec2b6cb345f75d39f90c0f8c65429f
Andreh,ayyad <andreh,ayad@gmail.com>
2024-11-26 19:03:10 +0300

    Text Editing and Understanding Widget

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp();
-
-  @override
-  Widget build(BuildContext context) => const MaterialApp(
-        home: Scaffold(
-          body: Center(child: const Text('Hello World!')),
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text(
+            'Flutter App',
+            style: TextStyle(color: Colors.white),
+          ),
         ),
-      );
+        body: Center(
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [Colors.red, Colors.pink]),
+              border: Border.all(color: Colors.white, width: 5),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                'Hello, World!',
+                style: TextStyle(fontSize: 24, color: Colors.white),
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
